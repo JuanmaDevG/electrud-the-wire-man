57780c3dfc2ee76ef4f3a49baac8a41f9ff8b696
ManuelJMazonM <manueljmazonm@gmail.com>
2025-10-29 13:57:54 +0100

    Organizado assets

---
+++ src/assets/enemy2.asm
@@ -0,0 +1,12 @@
+SECTION "Enemy2 tiles", ROM0
+enemy2_tiles::
+; Frame 1
+DB $00,$00,$1F,$1F,$25,$3F,$27,$3D
+DB $3F,$3F,$0C,$0C,$7F,$7F,$BD,$E1
+DB $AF,$F1,$DF,$DF,$CA,$CE,$5B,$5F
+DB $2F,$3F,$2B,$3B,$33,$33,$41,$41
+; Frame 2
+DB $00,$00,$1F,$1F,$25,$3F,$27,$3D
+DB $3F,$3F,$0C,$0C,$7F,$7F,$BD,$E1
+DB $AF,$F1,$DF,$DF,$CA,$CE,$5B,$5F
+DB $2F,$3F,$2A,$3E,$2E,$3E,$18,$18
